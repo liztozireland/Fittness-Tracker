41aff62e9076eb0b0b44473f5a24464b69ee65e2
Liz Ireland <liz.t.ireland@gmail.com>
2021-10-22 19:38:02 -0700

    adding server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const mongojs = require("mongojs");
+
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+// app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
