5f26b592da97cc7786368781a13aefea4a9c8c8d
Liz Ireland <liz.t.ireland@gmail.com>
2021-12-06 11:43:10 -0800

    API routes: Will log exercises on homepage but not on dashboard

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,65 @@
 const router = require("express").Router();
-const Workout = require("../models");
+const Workout = require("../models/workout");
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
 //put
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
 
 //get
 
 router.get("/api/workouts", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+  console.log(req.body);
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" }
+      }
+    }])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
